5f3f346cce5fb6415001a52f9d21ea60917712bf
shatteringlass <federico.pizzolo@gmail.com>
2024-12-04 14:39:48 +0100

    Complete day 1 part 2

--- day1.py
+++ day1.py
@@ -1,19 +1,48 @@
 DAY = 1
+PARTS = [1, 2]
+
+
+def parse_input():
+    with open(f"day{DAY}.txt", "r") as fp:
+        lines = fp.readlines()
+    return lines
+
+
+def part_one():
+    result = 0
+    lines = parse_input()
+    left, right = [sorted([row.split()[col] for row in lines]) for col in (0, -1)]
+
+    for pair_idx in range(len(lines)):
+        result += abs(int(left[pair_idx]) - int(right[pair_idx]))
+
+    return result
+
+
+def part_two():
+    result = 0
+    lines = parse_input()
+    left, right = [[int(row.split()[col]) for row in lines] for col in (0, -1)]
+
+    seen = {}
+
+    for item in left:
+        if item in seen:
+            continue
+        else:
+            seen[item] = sum(1 for x in right if x == item)
+        result += item * seen[item]
+    
+    return result
 
 
 def main():
-    result = 0
-
-    with open(f'day{DAY}.txt', 'r') as fp:
-        lines = fp.readlines()
-
-    left, right = [sorted([row.split()[col] for row in lines]) for col in (0,-1)]
-    
-    for pair_idx in range(len(lines)):
-        result += abs(int(left[pair_idx]) - int(right[pair_idx]))
-    
-    print(f"The result is {result}.")
+    for part in PARTS:
+        if part == 1:
+            print(f"Result for part {part} is {part_one()}")
+        elif part == 2:
+            print(f"Result for part {part} is {part_two()}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
