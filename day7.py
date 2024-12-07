2e2574b0bee5a4927320933e26583edeff1a3c96
shatteringlass <federico.pizzolo@gmail.com>
2024-12-07 23:02:16 +0100

    Day 7 - both parts

--- day7.py
+++ day7.py
@@ -1,30 +1,59 @@
-DAY = 3
+DAY = 7
 PARTS = [1, 2]
 
 
 def parse_input():
+    equations = []
     with open(f"day{DAY}.txt", "r") as fp:
-        lines = fp.readlines()
-    return lines
+        for line in fp.readlines():
+            p1, p2 = line.split(': ')
+            equations.append((int(p1), list(int(x) for x in p2.split())))
 
-def part_one(lines):
+    return equations
+
+
+def test_equation(target, numbers, current=0, concat=False):
+
+    if len(numbers) == 0:
+        return target == current
+
+    if current > target:
+        return False
+
+    return (
+        test_equation(target, numbers[1:], current + numbers[0], concat=concat)
+        or test_equation(target, numbers[1:], current*numbers[0], concat=concat)
+        or (concat and test_equation(target, numbers[1:], int(str(current) + str(numbers[0])), concat=concat))
+    )
+
+
+def part_one(equations):
     result = 0
+
+    for target, numbers in equations:
+        if test_equation(target, numbers):
+            result += target
 
     return result
 
-def part_two(lines):
+
+def part_two(equations):
     result = 0
+
+    for target, numbers in equations:
+        if test_equation(target, numbers, concat=True):
+            result += target
 
     return result
 
 
 def main():
-    lines = parse_input()
+    equations = parse_input()
     for part in PARTS:
         if part == 1:
-            print(f"Result for part {part} is {part_one(lines)}")
+            print(f"Result for part {part} is {part_one(equations)}")
         elif part == 2:
-            print(f"Result for part {part} is {part_two(lines)}")
+            print(f"Result for part {part} is {part_two(equations)}")
 
 
 if __name__ == "__main__":
