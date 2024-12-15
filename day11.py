14b8b48f31f2be67a51408c2d0330b52503f79b6
shatteringlass <federico.pizzolo@gmail.com>
2024-12-15 23:36:05 +0100

    Day 11

--- day11.py
+++ day11.py
@@ -1,30 +1,59 @@
-DAY = 3
+DAY = 11
 PARTS = [1, 2]
 
 
+def change(stone):
+    if stone == 0:
+        yield 1
+    elif len(txt:=str(stone)) % 2 == 0:
+        yield int(txt[:len(txt)//2])
+        yield int(txt[len(txt)//2:])
+    else:
+        yield stone*2024
+
+
 def parse_input():
+    stones = {}
     with open(f"day{DAY}.txt", "r") as fp:
-        lines = fp.readlines()
-    return lines
+        for x in fp.read().strip().split():
+            if int(x) in stones:
+                stones[int(x)] += 1
+            else:
+                stones[int(x)] = 1
+    return stones
 
-def part_one(lines):
-    result = 0
 
-    return result
+def blink(stones, times):
+    for blink in range(times):
+        new_stones = dict()
+        for stone, count in stones.items():
+            for chg in change(stone):
+                if chg in new_stones:
+                    new_stones[chg] += count
+                else:
+                    new_stones[chg] = count
+        stones = new_stones
 
-def part_two(lines):
-    result = 0
+    return stones
 
-    return result
+
+def part_one(stones):
+    stones = blink(stones, 25)
+    return sum(stones.values())
+
+
+def part_two(stones):
+    stones = blink(stones, 75)
+    return sum(stones.values())
 
 
 def main():
-    lines = parse_input()
+    stones = parse_input()
     for part in PARTS:
         if part == 1:
-            print(f"Result for part {part} is {part_one(lines)}")
+            print(f"Result for part {part} is {part_one(stones)}")
         elif part == 2:
-            print(f"Result for part {part} is {part_two(lines)}")
+            print(f"Result for part {part} is {part_two(stones)}")
 
 
 if __name__ == "__main__":
