cf526dbab1084f71f5d2fd502825a9a0daad214b
shatteringlass <federico.pizzolo@gmail.com>
2024-12-15 17:23:15 +0100

    Day 9 - pt.2

--- day9.py
+++ day9.py
@@ -2,7 +2,14 @@ DAY = 9
 PARTS = [1, 2]
 
 
-def parse_input():
+def parse_input(part):
+    if part == 1:
+        return parse_input_p1()
+    elif part == 2:
+        return parse_input_p2()
+
+
+def parse_input_p1():
     with open(f"day{DAY}.txt", "r") as fp:
         line = fp.read().strip()
         disk = [-1 for _ in range(len(line) * 9)]
@@ -15,6 +22,21 @@ def parse_input():
                 block_size -= 1
                 pos += 1
     return disk[:pos]
+
+
+def parse_input_p2():
+    with open(f"day{DAY}.txt", "r") as fp:
+        pos = 0
+        files, spaces = [], []
+        for idx, char in enumerate(fp.read().strip()):
+            size = int(char)
+            blocks = (pos, size)
+            if idx % 2 == 0:
+                files.append(blocks)
+            else:
+                spaces.append(blocks)
+            pos += size
+    return files, spaces
 
 
 def compact(disk):
@@ -35,7 +57,8 @@ def compact(disk):
 
 
 def checksum(disk):
-    return sum(i*int(d) for i,d in enumerate(disk))
+    return sum(i*int(d) for i, d in enumerate(disk))
+
 
 def part_one(disk):
     return checksum(compact(disk))
@@ -44,12 +67,38 @@ def part_one(disk):
 def part_two(disk):
     result = 0
 
-    return result
+    files, spaces = disk
+
+    for fidx in range(len(files) - 1, 1, -1):
+        file_start, file_size = files[fidx]
+        for sidx in range(len(spaces)):
+            space_start, space_size = spaces[sidx]
+            if (space_size and space_start < file_start and file_size <= space_size):
+                files[fidx] = (space_start, file_size)
+                spaces[sidx] = (space_start + file_size,
+                                space_size - file_size)
+                break
+
+    files = sorted([(idx, start, size) for idx, (start, size)
+                    in enumerate(files)], key=lambda x: x[1])
+
+    i = 0
+    s = []
+    while files:
+        file, *files = files
+        for gap in range(file[1] - i):
+            s.append('0')
+            i += 1
+        for rep in range(file[2]):
+            s.append(str(file[0]))
+            i += 1
+
+    return checksum(s)
 
 
 def main():
-    disk = parse_input()
     for part in PARTS:
+        disk = parse_input(part)
         if part == 1:
             print(f"Result for part {part} is {part_one(disk)}")
         elif part == 2:
