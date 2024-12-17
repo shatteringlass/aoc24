1366095ecafc19f9266150727534c070aefbd244
shatteringlass <federico.pizzolo@gmail.com>
2024-12-17 23:55:45 +0100

    Day 12 - pt.2 wip

--- day12.py
+++ day12.py
@@ -1,5 +1,15 @@
+import enum
+
+from collections import deque
 DAY = 12
 PARTS = [1, 2]
+
+
+class Direction(enum.Enum):
+    W = (0, -1)
+    E = (0, +1)
+    N = (-1, 0)
+    S = (1, 0)
 
 
 def parse_input():
@@ -11,26 +21,77 @@ def parse_input():
     return cells
 
 
-def find_regions(cells):
-    region = set()
-    yield region
+class Grid:
+    def __init__(self, cells):
+        self.cells = cells
 
+    def find_regions(self):
+        visited = set()
 
-def get_perimeter(region):
-    return None
+        for pos, char in self.cells.items():
+
+            if pos in visited:
+                continue
+
+            region = set()
+            plots = deque([pos])
+
+            while plots:
+
+                plot = plots.popleft()
+
+                if plot in visited:
+                    continue
+
+                new_char = self.cells.get(plot, "")
+                if new_char != char:
+                    continue
+
+                visited.add(plot)
+                region.add(plot)
+
+                for neighbor in self.neighbors(plot):
+                    plots.append(neighbor)
+
+            yield region
+
+    def neighbors(self, point, inner_only=True):
+        return [p for p in [tuple(sum(coord) for coord in zip(point, d.value)) for d in Direction] if (not(inner_only) or p in self.cells)]
+
+    def get_perimeter(self, region):
+        perimeter = 0
+        for point in region:
+            for neighbor in self.neighbors(point, inner_only=False):
+                if neighbor not in region:
+                    perimeter += 1
+        return perimeter
+
+    def count_sides(self, region):
+        sides = 0
+        for point in region:
+            for neighbor in self.neighbors(point, inner_only=False):
+                # TODO: figure out a proper way to count sides for a region
+                pass
+        return sides
 
 
 def part_one(cells):
     result = 0
-
-    print(cells)
-
+    g = Grid(cells)
+    for r in g.find_regions():
+        area = len(r)
+        perimeter = g.get_perimeter(r)
+        result += (area * perimeter)
     return result
 
 
 def part_two(cells):
     result = 0
-
+    g = Grid(cells)
+    for r in g.find_regions():
+        area = len(r)
+        sides = g.count_sides(r)
+        result += (area * sides)
     return result
 
 
